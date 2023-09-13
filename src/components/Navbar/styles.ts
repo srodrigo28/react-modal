fe22230c244ed94afb1a6d4609191847f908f545
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-09-13 11:15:47 -0300

    first commit

---
+++ src/components/Navbar/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+
+    color: #fff;
+    background-color: darkviolet;
+`
